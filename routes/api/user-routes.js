f860a4456a3ee261142421d75cda7b40ae180e22
Calista Mayer <calista.a.mayer@gmail.com>
2021-03-10 14:44:54 -0800

    create login route for authentication

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -49,6 +49,30 @@ router.post('/', (req, res) => {
         });
 });
 
+// login route
+router.post('/login', (req, res) => {
+    // query operation expects req.body to contain username and password strings
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => { 
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+});
+
 // update user by id
 router.put('/:id', (req, res) => {
     // expects req.body to contain username, email and password strings
