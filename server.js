8950abc821e32f4e19422414f3a05f1a8cdfb0c5
Calista Mayer <calista.a.mayer@gmail.com>
2021-03-16 15:49:14 -0700

    login and logout functionality

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 
+const session = require('express-session');
+// require('dotenv').config();
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'secret56',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -11,6 +26,8 @@ const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
